b875636fdfe0004e0578e8eccb99f4c94b34e2a6
salomechik <salome.chikvaidze.05@gmail.com>
2023-07-04 00:27:57 -0700

    update JS code

--- index.js
+++ index.js
@@ -1,75 +1,77 @@
-const bill = document.querySelector(".billinput")
-const people = document.querySelector(".billinput2")
-const button = document.querySelectorAll(".greenbutton")
-const custom = document.querySelector(".whitebutton")
-const sum = document.querySelector(".greeninput")
-const total = document.querySelector(".second")
-const reset = document.querySelector(".reset")
-const error = document.querySelector(".red")
+const bill = document.querySelector(".billinput");
+const people = document.querySelector(".billinput2");
+const button = document.querySelectorAll(".greenbutton");
+const custom = document.querySelector(".whitebutton");
+const sum = document.querySelector(".greeninput");
+const total = document.querySelector(".second");
+const reset = document.querySelector(".reset");
+const error = document.querySelector(".red");
+const firsterror = document.querySelector(".rederror");
 
-let x = 0.0
-bill.addEventListener("input", getBill)
-
-
+let x = 0.0;
+bill.addEventListener("input", getBill);
 
 function getBill() {
-   
-    calculator(bill.value, x, peopleValue)
+  if (bill.value == "-") {
+    bill.value = "";
+  }
+  calculator(bill.value, x, peopleValue);
+  if (bill.value > 0) {
+    firsterror.style.display = "none";
+  } else {
+    firsterror.style.display = "block";
+  }
 }
-people.addEventListener("input", numberPeople)
+people.addEventListener("input", numberPeople);
 
-  let peopleValue = 0.0
+let peopleValue = 0.0;
 
 function numberPeople() {
-    peopleValue = people.value;
-    calculator(bill.value, x, peopleValue)
+  peopleValue = people.value;
 
-    if (people.value > 0) {
-      error.style.display = "none"
-    } else {
-      error.style.display = "block"
-    };
+  if (people.value == "-") {
+    people.value = "";
+  }
 
+  calculator(bill.value, x, peopleValue);
+
+  if (people.value > 0) {
+    error.style.display = "none";
+  } else {
+    error.style.display = "block";
+  }
 }
 
-custom.addEventListener("input", getCustom)
+custom.addEventListener("input", getCustom);
 
-   let customValue = 0.0
+let customValue = 0.0;
 
- function getCustom() {
-    x = custom.value;
-
+function getCustom() {
+  x = custom.value;
 }
-
-
-
 button.forEach((button, index) => {
   button.addEventListener("click", () => {
-
-    x = button.textContent.replace("%", "")
-    calculator(bill.value, x, peopleValue)
-  })
-})
+    x = button.textContent.replace("%", "");
+    calculator(bill.value, x, peopleValue);
+  });
+});
 
 function calculator(billValue, x, peopleValue) {
   let BillNum = Number(billValue);
   let peopleNum = Number(peopleValue);
   let xNum = Number(x);
 
-  if (xNum && BillNum && peopleNum ){
+  if (xNum && BillNum && peopleNum) {
+    const toFixed = (BillNum * (xNum / 100)) / peopleNum;
+    const tofixed2 = BillNum / peopleNum + toFixed;
 
-  const toFixed = (BillNum * (xNum / 100)) / peopleNum;
-  const tofixed2 = (BillNum/peopleNum) + toFixed;
-
-
-  sum.textContent = toFixed.toFixed(2);
-  total.textContent = tofixed2.toFixed(2);
-  return toFixed;
+    sum.textContent = toFixed.toFixed(2);
+    total.textContent = tofixed2.toFixed(2);
+    return toFixed;
   }
-
 }
 
- reset.addEventListener("click", function() {
+reset.addEventListener("click", function () {
   changeText();
 });
 
